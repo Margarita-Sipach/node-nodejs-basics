027201e1d34817afd14f9f8e1cbf2d3cabb92d4a
Margarita <Margarita>
2023-06-19 22:23:29 +0300

    feat: add fs/read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const fileName = join(__dirname, "files", "fileToRead.txt");
+  fs.readFile(fileName, { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(data);
+    }
+  });
 };
 
 await read();

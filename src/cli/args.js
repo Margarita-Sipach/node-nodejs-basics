53babfe22c221744ca8a1d02610c343b58c3daf7
Margarita <Margarita>
2023-06-19 22:25:01 +0300

    feat: add cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  const sortArgs = args.slice(args.findIndex((item) => item.startsWith("--")));
+  for (let i = 0; i < sortArgs.length; i += 2)
+    console.log(
+      `${sortArgs[i].slice(2)} is ${sortArgs[i + 1]}${
+        i < sortArgs.length - 2 ? "," : ""
+      }`
+    );
 };
 
 parseArgs();

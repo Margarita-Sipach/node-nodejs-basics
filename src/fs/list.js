3d0a8debd486d52bf7bd6b2df1f998abd5c09887
Margarita <Margarita>
2023-06-19 22:23:16 +0300

    feat: add fs/list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const folderName = join(__dirname, "files");
+  fs.readdir(folderName, (err, filenames) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }	else{
+			console.log(filenames.reduce((acc, filename) => `${acc}, ${filename}`))
+		}
+  });
 };
 
 await list();
